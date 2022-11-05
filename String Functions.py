6da39454595e9c492d2de25b8305ac8b48a9a840
AbhishekThings <107426102+AbhishekThings@users.noreply.github.com>
2022-11-05 12:48:32 +0530

    A Basic String functions code in Python.

---
+++ String Functions.py
@@ -0,0 +1,11 @@
+a = 'abhishek'
+print(a)
+b = ''' Abhishek.....
+                Things ''' 
+print(b )
+
+print(len(b))
+print(a[3])
+print(a[0].upper() + a[1: ] )
+print('Abhi "RAM" Mpss ')
+print(a[0: len(a)-1] + a[-1].upper())
