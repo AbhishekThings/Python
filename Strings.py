a37e168db68569d6849e22bb97a86105ec8ce59b
AbhishekThings <107426102+AbhishekThings@users.noreply.github.com>
2022-11-05 12:53:20 +0530

    Hello World and Basic String Function files.

---
+++ Strings.py
@@ -0,0 +1,13 @@
+a = 'Abhishek'
+b = 'MPSS'
+print(a)
+print(b)
+c = a + b
+print(c)
+d = print(a[0].lower() + a[1 : ].upper() )
+e = ''' Abhishek 
+                 Thinks '''
+print(e)
+f = len(a)
+print(f)
+g = print(a[0:3].lower() +a[4:7].upper() + a[7].lower())
