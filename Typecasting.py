f2c91be8cb9733c3556ebacc1ada89a5966978d9
AbhishekThings <107426102+AbhishekThings@users.noreply.github.com>
2022-11-07 11:23:46 +0530

    Typecasting operators

---
+++ Typecasting.py
@@ -0,0 +1,18 @@
+a = 'Abhi' * 3
+print(a)
+
+b =int(10.726)
+print(b)
+
+c = float(10)
+print(c)
+
+d = str(10)
+print(d)
+
+e = bool('Abhi')
+print(e)
+
+f = int('a')
+print(f)
+#The above code gives an error as a string value cannot be converted to int.
