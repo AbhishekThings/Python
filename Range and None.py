bfa2f5cad63d9158dad1b10b866bf158aace4171
AbhishekThings <107426102+AbhishekThings@users.noreply.github.com>
2022-11-10 12:05:10 +0530

    Range and None DT code with explanation

---
+++ Range and None.py
@@ -0,0 +1,42 @@
+#Example on Range Data Type
+
+a = range (10)
+print(type(a))
+print(a)
+
+for x in a:
+	print(x)
+b = range(1,20,2)
+print(b)
+for y in b:
+	print(y)
+
+c = range(20,1,-2)
+for z in c:
+	print(z)
+
+print(a[1])
+print(b[2])
+print(c[3])
+
+#None Data Type
+
+d = 20
+print(type(d))
+print(d)
+print(id(d))
+d = None
+print(type(d))
+print(d)
+print(id(d))
+
+#Even if we have multiple None with different variables it will target to one none object with same id
+
+#Examples on Special Characters
+
+print('Abhishek\n Things ')
+print('Abhishek\t Rocks ')
+print('Abhishek\r Smiles')
+print('Abhishek\' Thinks')
+print('Abhishek\"'' Memes')
+
