997d47031faf55c90c839259da6505f0002918a3
AbhishekThings <107426102+AbhishekThings@users.noreply.github.com>
2022-11-22 12:40:42 +0530

    A simple Module file for calculating.

---
+++ Module.py
@@ -0,0 +1,12 @@
+a = 0
+b = 0
+def add(a , b ):
+	print( a +b)
+def prod(a,b ):
+	print( a * b )
+def sub(a , b):
+	print( a - b )
+def div (a,b):
+	print( a / b )
+def floor(a ,b ):
+	print(a // b)
